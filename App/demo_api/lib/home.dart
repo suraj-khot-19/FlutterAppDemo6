a31cc785f7e062c284b8d8689b07c72c13378482
suraj khot <surajkhot19082001@gmail.com>
2024-04-06 12:08:19 +0530

    printing no of user using http:success

---
+++ App/demo_api/lib/home.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:demo_api/Models/post_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //list
+  List<PostsModel> postList = [];
+  //future function
+  Future<List<PostsModel>> getPostApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Api Integration"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Text("Loading...");
+                } else {
+                  return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: (context, index) {
+                        return Text(index.toString());
+                      });
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
